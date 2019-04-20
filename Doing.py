0eb96e2374a2f97d18969775b75d13072101d1b5
Patrick Go <patrickwilliamgo@gmail.com>
2019-04-20 13:07:44 -0700

    Create Doing.py

---
+++ Doing.py
@@ -0,0 +1,21 @@
+#This function is for addition
+def add(x, y):
+   return x + y
+
+#Function for subtraction
+def subtract(x, y):
+   return x - y
+
+#Function for multiplication
+def multiply(x, y):
+   return x * y
+
+#Function for division
+def division(x, y):
+   return x / y
+print("Welcome to Patrick's calculator! ")
+print('\n' * 4)
+print("Enter 1 for addition ")
+print("Enter 2 for subtraction ")
+print("Enter 3 for multiplication ")
+print("Enter 4 for division ")

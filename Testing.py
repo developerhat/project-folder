fef32a1a6f84b9c55f20d0c8e53921d950021817
Patrick Go <patrickwilliamgo@gmail.com>
2019-04-30 16:45:36 -0700

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -256,3 +256,16 @@ def master_yoda(a):
 
 def almost_there(num3):
     return (abs(100 - num3) <=10) or (abs(200-num3) <=10)
+
+def has_33(nums):
+    for i in range(0, len(nums)-1):
+        if nums[i] == 3 and nums[i+1] == 3:
+            return True
+
+    return False
+
+def paper_doll(text):
+    textResult = ''
+    for char in text:
+        textResult += char*3
+    return textResult

92226cd8ddc954ff30603fcca1bc9beb3efdd32d
Patrick Go <patrickwilliamgo@gmail.com>
2019-05-02 12:26:31 -0700

    Update testing3.py

--- testing3.py
+++ testing3.py
@@ -1,1 +1,21 @@
+#usr/bin/python
 
+
+#practicing from practicepython.org practice problems
+
+userNum = int(input("Give me a number:  "))
+
+if userNum % 4==0:
+    print('this number is divisible by 4')
+elif userNum % 2==0:
+    print('this number is even')
+else:
+    print('this number is odd')
+
+#This is for practice problem list list_ends
+#Can return the last value, but not the first one
+def list_ends():
+    a = [5, 10, 15, 20, 25]
+
+    new_list = a.pop()
+    return new_list

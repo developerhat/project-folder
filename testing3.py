aaacf1389eb56f8500ed51a262a3d028d886d8a8
Patrick Go <patrickwilliamgo@gmail.com>
2019-05-06 17:19:19 -0700

    Update testing3.py

--- testing3.py
+++ testing3.py
@@ -17,5 +17,25 @@ else:
 def list_ends():
     a = [5, 10, 15, 20, 25]
 
-    new_list = a.pop()
+    new_list = a[0],a.pop()
     return new_list
+
+def list_overlap():
+     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+     b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+     #Exercise number 5 for practicepython.org
+     #need to use in operator to check
+
+def max_three():
+    Num1 = int(input('Number 1: '))
+    Num2 = int(input('Number 2: '))
+    Num3 = int(input('Number 3: '))
+
+    if Num3 > Num2 and Num1:
+        return Num3
+    elif Num2 > Num1 and Num3:
+        return Num2
+    elif Num1 > Num3 and Num2:
+        return Num1
+    #Easy way to do it below. Do it yourself above. Try using 6, logic is flawed need to modify
+    #return max(Num1, Num2, Num3)

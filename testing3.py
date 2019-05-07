6670eed206146462ff3dd9133788a8caa7edff85
Patrick Go <patrickwilliamgo@gmail.com>
2019-05-07 16:53:19 -0700

    Update testing3.py

--- testing3.py
+++ testing3.py
@@ -39,3 +39,62 @@ def max_three():
         return Num1
     #Easy way to do it below. Do it yourself above. Try using 6, logic is flawed need to modify
     #return max(Num1, Num2, Num3)
+
+#real_password = "unsafepassword"
+#user_password = str(input("Enter the password: "))
+#Password checker
+#while user_password != real_password:
+#    user_password = input("Enter the password: ")
+#print("You may enter!")
+
+def pig_latin(word):
+
+    first_letter = word[0]
+    #check for vowel
+    if first_letter in 'abcdefghijklmnopqrstuvwxyz':
+        pig_word = word + 'ay'
+    else:
+        pig_word = word[1:] + first_letter + 'ay'
+
+    return pig_word
+
+print(pig_latin('hi there'))
+
+#Checks for whether or not 2 numbers are 10, make 10, or exceed 10
+#This works! Only numbers sum 10, or are 10
+def makes10(a, b):
+    if a + b > 10:
+        return False
+    elif a + b < 10:
+        return False
+    elif a or b == 10:
+        return True
+    elif a + b == 10:
+        return True
+
+#From codingbat warmup problem
+#Returning absolute difference for given number
+def diff21(n):
+    if n <= 21:
+        return 21 - n
+        return abs(n)
+    elif n > 21:
+        return (n - 21) * 2
+
+#this is the complex way, and it's broken. Verbose
+def sleep_in(weekday, vacation):
+    if weekday == True:
+        return False
+    elif vacation == True:
+        return True
+    elif vacation == False:
+        return False
+    elif weekday == False:
+        return True
+
+#this is the advanced way of doing it, although not as verbose
+def sleep_in2(weekday, vacation):
+    if not weekday or vacation:
+        return True
+    else:
+        return False

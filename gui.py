352054fdbd7b9274f25464446988aeb915e930d7
Patrick Go <patrickwilliamgo@gmail.com>
2019-07-18 12:05:02 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -18,27 +18,6 @@ def print_name():
 def hello_world():
     world.set('Hello World!')
 
-def cal_price():
-    pass
-
-
-#label1 = Label(root, text = 'P1')
-#label1.place(x=100, y=50)
-
-#label2 = Label(root, text = "P2")
-#label2.place(x=50,y=50)
-
-#label3 = Label(root, text = "P3")
-#label3.place(x=50, y=100)
-#Placed P1-4 in all 4 corners of the program
-#label4 = Label(root, text = 'P4')
-#label4.place(x=100, y=100)
-
-#label5 = Label(root, textvariable=name)
-#label5.place(x = 75, y = 150)
-
-#label6 = Label(root, textvariable=world)
-#label6.place(x=150, y = 150)
 
 #btn1 = Button(root, text = "Click me", command = lambda: print_name())
 #btn1.place(x = 10, y =0)
@@ -47,6 +26,17 @@ def cal_price():
 #btn2.place(x = 100, y = 0)
 
 v = IntVar()
+
+res = IntVar()
+
+def cal_price():
+    value = int(v.get())
+    if value == 0:
+        res.set(int(inWindow2.get()) * 4)
+    elif value == 1:
+        res.set(int(inWindow2.get()) * 6)
+    elif value == 2:
+        res.set(int(inWindow2.get()) * 10)
 
 
 label = Label(root, text= "Choose an Item", bg="gray77")
@@ -61,7 +51,7 @@ r_btn2.place(x=5, y=60)
 r_btn3 = Radiobutton(root, text = "Mango", bg="gray77", variable = v, value=2)
 r_btn3.place(x=5, y=90)
 
-inText = Label(root, text="Result:", bg="gray77")
+inText = Label(root, text="Price: ", bg="gray77")
 inText.place(x=0, y=130)
 
 inWindow = Entry(root, width = 20)
@@ -71,9 +61,8 @@ inWindow2 = Entry(root, width = 20)
 inWindow2.place(x=70, y=160)
 
 
-r_btn4 = Button(root, text="Calculate", highlightbackground = 'gray77', command = lambda: cal_price())
+r_btn4 = Button(root, text="Cal: ", highlightbackground = 'gray77', command = lambda: cal_price())
 r_btn4.place(x=0, y = 160)
-
 
 
 

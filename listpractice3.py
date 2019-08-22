519f0e5c732a323f66b885d2744734c57df685d3
Patrick Go <patrickwilliamgo@gmail.com>
2019-08-22 09:35:13 -0700

    Create listpractice3.py

---
+++ listpractice3.py
@@ -0,0 +1,6 @@
+#Python practice with lists 12/12/17
+
+pizzaNames= ['pepperoni', 'bacon', 'cheese']
+
+for i in pizzaNames:
+    print("One of my favorite pizza flavors is.. " + str(pizzaNames[i]))

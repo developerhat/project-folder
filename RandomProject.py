69c9e466a9bb6a78d759546bf14213ade3ef4857
Patrick Go <patrickwilliamgo@gmail.com>
2019-08-22 09:35:09 -0700

    Create RandomProject.py

---
+++ RandomProject.py
@@ -0,0 +1,15 @@
+#usr/bin/python
+import os.path
+
+
+firstVar = str(input('Enter your name: '))
+
+print('\n')
+
+print('Welcome to the Random Program, ' + firstVar,'!')
+
+print('\n')
+
+print('Here are your options: ')
+
+print('1: If you would like to open a page, choose this')

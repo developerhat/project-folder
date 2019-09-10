d3ac421b56c441ca2b0ea7a02f0e2f02323e9dae
Patrick Go <patrickwilliamgo@gmail.com>
2019-09-09 20:47:13 -0700

    Create patrickgoLab1.py

---
+++ patrickgoLab1.py
@@ -0,0 +1,43 @@
+###########################################################
+# CIS 117 Python Programming: Lab #1
+#
+# Compiler, Tabs, Indentation, User Output
+#
+###########################################################
+
+#usr/bin/python
+
+#The code below prints 4 statements: Hello world, last name, G number---
+#-- and details from course syllabus & Coding Standards for Assignments
+print('Hello World!')
+print('My last name is Go')
+print('My G number is G01037101')
+print('\n')
+
+
+#2 details from syllabus
+print('One of our student learning outcomes is to develop interactive Python programs through object-oriented programming. ')
+print('There are 12 assignments throughout this class. Assignments are due every Tuesday at 11:59pm.')
+print('\n')
+
+
+#3 details from Coding Standards
+print('One of our required coding standards is to use 4 spaces for indentation.')
+print('Submitted source code must contain comments in between blocks of code that describe what a program does.')
+print('All files must contain an introductino header containing lab number and title for the file.')
+print('\n')
+
+#Program output:
+
+#Hello World!
+#My last name is Go
+#My G number is G01037101
+
+
+#One of our student learning outcomes is to develop interactive Python programs through object-oriented programming.
+#There are 12 assignments throughout this class. Assignments are due every Tuesday at 11:59pm.
+
+
+#One of our required coding standards is to use 4 spaces for indentation.
+#Submitted source code must contain comments in between blocks of code that describe what a program does.
+#All files must contain an introductino header containing lab number and title for the file.

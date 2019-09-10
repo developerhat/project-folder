69d2d7b99f34d6a6228f29ce8dc94c29b11b93c1
Patrick Go <patrickwilliamgo@gmail.com>
2019-09-10 16:21:41 -0700

    Update patrickgoLab3.py

--- patrickgoLab3.py
+++ patrickgoLab3.py
@@ -9,78 +9,62 @@
 
 #usr/bin/python
 
-
-#def main():
-#grocery_bill = input('What is the total amount of your grocery bill? ')
-#while True:
-#    if grocery_bill.isdigit():
-#        grocery_bill = int(grocery_bill)
-#        print('you entered: ', grocery_bill)
-#        break
-
+#Defining main function
 def main():
+    """
+    Ask user for grocery bill amount, then calculate & print coupon amount.
+    """
+    #While loop to begin program
     while True:
-        grocery_bill = input('What is the total amount of your grocery bill? ')
+        user_input = input('What is the total amount of your grocery bill? ' )
         try:
-            float(grocery_bill)
-            return True
-        except ValueError:
-            return False
-        if grocery_bill.isdigit():
-            grocery_bill = float(grocery_bill)
-            print('you entered: ',grocery_bill)
+            #Store user input as a float
+            grocery_bill = float(user_input)
+            #If input is negative, prompt user again
+            if grocery_bill < 0:
+                continue
             break
-        elif type(grocery_bill) == float():
-            grocery_bill = float(grocery_bill)
-            print('you entered: ',grocery_bill)
-            break
+        except ValueError as e:
+            continue
+    print('you entered: ', round(grocery_bill, 2))
+    #Processing user input below, depending on amount given
     if grocery_bill <= 10:
         print('Sorry, you get no coupon!')
     elif grocery_bill > 10 and grocery_bill < 60:
+        #The calculation below converts percentage to decimal
         dollar_amnt = grocery_bill * 8 / 100.0
-        print('You receive an 8% off coupon. Dollar amount: $',dollar_amnt)
+        #Round() is used to round value to 2 decimal places
+        print('You receive an 8% off coupon. Amount: $',round(dollar_amnt, 2))
     elif grocery_bill >= 60 and grocery_bill <= 150:
         dollar_amnt1 = grocery_bill * 10 / 100.0
-        print('You receive a 10% off coupon. Dollar amount: $',dollar_amnt1)
+        print('You receive a 10% off coupon. Amount: $',round(dollar_amnt1, 2))
     elif grocery_bill >= 150 and grocery_bill <= 210:
         dollar_amnt2 = grocery_bill * 12 / 100.0
-        print('You receive a 12% off coupon. Dollar amount: $',dollar_amnt2)
+        print('You receive a 12% off coupon. Amount: $',round(dollar_amnt2, 2))
     elif grocery_bill >= 210:
         dollar_amnt3 = grocery_bill * 14 / 100.0
-        print('You receive a 14% off coupon. Dollar amount: $', dollar_amnt3)
+        print('You receive a 14% off coupon. Amount: $',round(dollar_amnt3, 2))
 
 
+#For loop to execute function 5 times
 for x in range(5):
     main()
 
+#Program output:
 
-
-#TRY USING ERICS CODE TO DO THIS!
-# Will need to import over all other stuff
-#def main1():
-    """
-    Asks user for grocery bill amount, then print coupon amount eligible.
-    """
-#    grocery_bill = -1
-#    while grocery_bill < 0:
-#        user_input = input('What is the total amount of your grocery bill? ' )
-#        try:
-#            grocery_bill = float(user_input)
-#            break
-#        except ValueError as e:
-#            continue
-#    print('you entered: ', grocery_bill)
-
-#main1()
-
-#def main():
-#grocery_bill = 0
-#while True:
-#	   user_input = input('What is the total amount of your grocery bill? ')
-#	   try:
-#	       grocery_bill = float(user_input)
-#	   except ValueError as e:
-#	       continue
-#	       break
-
-#print('you entered: ', grocery_bill)
+#What is the total amount of your grocery bill? -2
+#What is the total amount of your grocery bill? 30.35
+#you entered:  30.35
+#You receive an 8% off coupon. Amount: $ 2.43
+#What is the total amount of your grocery bill? 80.88
+#you entered:  80.88
+#You receive a 10% off coupon. Amount: $ 8.09
+#What is the total amount of your grocery bill? 171.33
+#you entered:  171.33
+#You receive a 12% off coupon. Amount: $ 20.56
+#What is the total amount of your grocery bill? 222.33
+#you entered:  222.33
+#You receive a 14% off coupon. Amount: $ 31.13
+#What is the total amount of your grocery bill? 8
+#you entered:  8.0
+#Sorry, you get no coupon!

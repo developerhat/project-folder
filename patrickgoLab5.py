b3e6b0fb1ba5ea86f38e0aadae20ea2d9a7dfe1d
Patrick Go <patrickwilliamgo@gmail.com>
2019-09-26 16:45:11 -0700

    Create patrickgoLab5.py

---
+++ patrickgoLab5.py
@@ -0,0 +1,19 @@
+###########################################################
+# CIS 117 Python Programming: Lab #5
+#
+# Execution Control Structures, File Encoding, Using Containers
+#
+#
+###########################################################
+
+#usr/bin/python
+
+#Start by validating user data with a while loop
+
+while True:
+    user_prompt = str(input('Please enter the name of input file: '))
+    try:
+        if user_prompt == 't5.dat':
+            break
+    except:
+        continue

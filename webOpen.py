b65d2cd1a7b4994bdd62c737381a6ffb7bc31060
Patrick Go <patrickwilliamgo@gmail.com>
2019-10-08 16:26:43 -0700

    Create webOpen.py

---
+++ webOpen.py
@@ -0,0 +1,13 @@
+#usr/bin/python
+
+
+import webbrowser
+
+#Script that opens web downloads Python 3
+webbrowser.open('https://slack.com/downloads', new =1, autoraise = True)
+
+webbrowser.open('https://www.box.com/resources/downloads/drive', new =1, autoraise = True)
+
+webbrowser.open('https://www.google.com/chrome/', new =1, autoraise = True)
+
+webbrowser.open('https://zoom.us/download', new =1, autoraise = True)

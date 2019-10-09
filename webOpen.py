d32f3513a26dcfc89ca3b5897d877887a386eadb
Patrick Go <patrickwilliamgo@gmail.com>
2019-10-09 15:56:23 -0700

    Update webOpen.py

--- webOpen.py
+++ webOpen.py
@@ -1,7 +1,13 @@
 #usr/bin/python
 
+#NOTE: Script is optimized to run on Python 3.
+#Working on converting code to run on Python 2.7 (default for Macs as of 10/07/19)
 
+#Importing webbrowser module
 import webbrowser
+
+#This script opens the URL's listed below.
+#Each link will open in a new tab
 
 #Script that opens web downloads Python 3
 webbrowser.open('https://slack.com/downloads', new =1, autoraise = True)

d037ab0497950c5973dee4c297e7fab1fa86a7f3
Patrick Go <patrickwilliamgo@gmail.com>
2019-10-24 12:46:54 -0700

    Update ATBSWP.py

--- ATBSWP.py
+++ ATBSWP.py
@@ -3,7 +3,9 @@
 
 #Practicing using ATBSWP
 
-#This program says hello, asks for age, and displays length + age in a year 
+import pyperclip
+
+#This program says hello, asks for age, and displays length + age in a year
 user_input = str(input('Hello! What is your name? '))
 user_age = int(input('What is your age? '))
 
@@ -16,3 +18,17 @@ else:
     print('great to meet you!')
 
 print('You will be ' + str((user_age +1)) + ' in one year. Congrats!')
+
+
+#The below code uses range(len(list)) to print out all items in the list
+cars = ['VW GTI', 'Subaru BRZ', 'Honda Civic Si']
+for i in range(len(cars)):
+    print('Index ' + str(i) + ' of this list is: ' + cars[i])
+
+
+copy_clipboard = str(input('What would you like to copy on the clipboard? '))
+
+pyperclip.copy(copy_clipboard)
+paste_clipboard = pyperclip.paste()
+
+print(paste_clipboard)

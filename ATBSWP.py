1c371a98f69670da223eea97f89678c73df06fbb
Patrick Go <patrickwilliamgo@gmail.com>
2019-11-06 21:52:31 -0800

    Update ATBSWP.py

--- ATBSWP.py
+++ ATBSWP.py
@@ -3,7 +3,7 @@
 
 #Practicing using ATBSWP
 
-#This program says hello, asks for age, and displays length + age in a year 
+#This program says hello, asks for age, and displays length + age in a year
 user_input = str(input('Hello! What is your name? '))
 user_age = int(input('What is your age? '))
 
@@ -16,3 +16,16 @@ else:
     print('great to meet you!')
 
 print('You will be ' + str((user_age +1)) + ' in one year. Congrats!')
+
+
+#Messing round w Try/exception
+#Trying to figure out what's going on here
+def exception():
+    user_input = str(input("What's your name? "))
+    try:
+        print(user_input)
+        print(x)
+    except:
+        print('No value for X')
+    finally:
+        print("Hi!")

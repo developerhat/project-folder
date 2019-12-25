610e18ff1a0ee19caf76dd31d6f7e252a012c909
Patrick Go <patrickwilliamgo@gmail.com>
2019-12-24 16:38:09 -0800

    Update MoveMouse.py

--- MoveMouse.py
+++ MoveMouse.py
@@ -1,3 +1,17 @@
 #usr/bin/python
 
-#Moving mouse script
+#Using web browser to scrape sites
+
+import webbrowser
+import sys
+import pyperclip
+
+user_input = str(input(sys.argv)) # Parameters will be ['mapit.py', '870', 'Valencia', 'St']
+
+#Check if command line arguments were passed
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:]) #Doing formatting here
+else:
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)

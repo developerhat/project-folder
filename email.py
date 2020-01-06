9d2d90cbfb9e00f54d3f79fdacc8a1bc7b606a81
Patrick Go <patrickwilliamgo@gmail.com>
2020-01-06 09:42:49 -0800

    Update email.py

--- email.py
+++ email.py
@@ -1,6 +1,8 @@
 import smtplib
 #SERVER = "localhost"
 
+#This program works, but need to do it all in IDLE for some reason?
+
 connection = smtplib.SMTP('smtp.gmail.com', 587)
 
 connection.ehlo()
@@ -9,6 +11,6 @@ connection.starttls()
 
 connection.login('pwgbusiness@gmail.com', 'M0neytalkss')
 
-connection.sendmail('pwgbusiness@gmail.com', 'bprimer@wearexyz.com', 'Subject: Testing \n\n Dear Hello, \n, I am testing sending email with Python. It rocks! \n\n -Pat')
+connection.sendmail('pwgbusiness@gmail.com', 'brittprimer@gmail.com', 'Subject: Testing \n\n Dear Hello, \n, I am testing sending email with Python. It rocks! \n\n -Pat')
 
 connection.quit()

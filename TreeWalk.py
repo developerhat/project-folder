14d293466e9ec41237e0d2058bd311986d48b0f9
Patrick Go <patrickwilliamgo@gmail.com>
2020-01-16 11:24:20 -0800

    Create TreeWalk.py

---
+++ TreeWalk.py
@@ -0,0 +1,8 @@
+#Walking directory tree practice
+import os
+
+for root, dirs, files in os.walk(".", topdown=False):
+   for name in files:
+      print(os.path.join(root, name))
+   for name in dirs:
+      print(os.path.join(root, name))

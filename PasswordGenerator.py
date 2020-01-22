85ce1b23a8d714e7d0ee6e4ea8fcc76dbaf40fb4
Patrick Go <patrickwilliamgo@gmail.com>
2020-01-21 16:52:28 -0800

    Create PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,16 @@
+#Password generator program
+#Wow pretty basic! Got it to work w minimal help
+import random
+
+print("Hello! Welcome to Patrick's password generator!")
+print("\n")
+word_list = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+
+passlength = 0
+print('For security purposes, your password must exceed 8 characters.')
+
+while passlength <= 7:
+    passlength = int(input('How long would you like this password to be? '))
+else:
+    generated_password = ''.join(random.sample(word_list,passlength))
+    print("Thanks for using me! Here's your super secure password: ", generated_password)

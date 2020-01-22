8803d1ef4e0fb0f092a8dae3905cdbd76c20fa1a
Patrick Go <patrickwilliamgo@gmail.com>
2020-01-21 17:42:46 -0800

    Update NumberGenerator.py

--- NumberGenerator.py
+++ NumberGenerator.py
@@ -6,11 +6,15 @@ print("Welcome to the Random Generator Program!")
 print("This program will generate a random number. Your objective is to correctly guess this number")
 print("\n")
 
-user_answer = int(input("I'm thinking of a number between 1 and 5.. can you guess? "))
-
+#Generate the integer here
 integer = random.randint(1,5)
+user_answer = 0 #Store variable
 
-if user_answer == integer:
-    print("You got it! Your input: ", user_answer, "My input: ", integer)
-else:
-    print("Nope! Not that time. Your input: ", user_answer, "My input: ", integer)
+#Wow!! Got this on my own w no help!! holy shit!
+while integer != user_answer:
+    user_answer = int(input("I'm thinking of a number between 1 and 5.. can you guess? "))
+    if user_answer == integer:
+        print("You got it! Your input: ", user_answer, "My input: ", integer)
+        True
+    else:
+        print("Nope! Not that time. Your input: ", user_answer, "My input: ", integer)

ee99ff23f300d12e9bc1ec0adebbca63a62ec8cc
Patrick Go <patrickwilliamgo@gmail.com>
2020-01-23 12:45:29 -0800

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -11,6 +11,8 @@ print('For security purposes, your password must exceed 8 characters.')
 
 while passlength <= 7:
     passlength = int(input('How long would you like this password to be? '))
+    if passlength <=7:
+        print('We need more than 8 chars fam!')
 else:
     generated_password = ''.join(random.sample(word_list,passlength))
     print("Thanks for using me! Here's your super secure password: ", generated_password)

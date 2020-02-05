c251bcd63cde1a1d7acb865f2daf0a54480be025
Patrick Go <patrickwilliamgo@gmail.com>
2020-02-04 17:00:38 -0800

    Update CreateFolder.py

--- CreateFolder.py
+++ CreateFolder.py
@@ -3,4 +3,9 @@ import os
 
 path = "/Users/patrickgo/Desktop/Misc/NetDevEng"
 
-os.mkdir(path)
+#os.mkdir(path)
+
+print(os.getcwd())
+print("\n")
+print("All of the files & folders in this directory are: \n",os.listdir())
+#Prints current working directory + sublists & files

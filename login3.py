0f76d20bf43305cae4c46bbac32d06ff1e649360
Patrick Go <patrickwilliamgo@gmail.com>
2020-02-09 13:33:58 -0800

    Update login3.py

--- login3.py
+++ login3.py
@@ -13,17 +13,19 @@ user_list.append(user_input) #Whatever they enter, store in list
 pass_input = input('Awesome! Now, time to create a password: ')
 pass_list.append(pass_input)
 
-username = str(input("Great! Now it's time to login. Please enter username: "))
+#username = str(input("Great! Now it's time to login. Please enter username: "))
+attempts_counter = 0
 
-
-#Need to fix this loop 
-while username not in user_list:
-    username = str(input("Please enter username: "))
+while attempts_counter < 3:
+    username = str(input("Time to login! Please enter username: "))
     password = input("Now password: ")
-    if username and password in user_list:
-        break
+    if username in user_list and password in pass_list:
         print("Successfully authenticated! Hooray! :) ")
-        True
-    else:
-        print("Please try again! ")
         False
+        break
+    else:
+        print("Invalid credentials. Please try again! 3 attempts max. ")
+        attempts_counter +=1 #Adding for each attempt
+        True
+        if attempts_counter == 3:
+            print("Maximum attempts reached! Please try again later. ")

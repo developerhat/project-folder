c94d29bb8dcdcab456c0646babf67ecaa6571e88
Patrick Go <patrickwilliamgo@gmail.com>
2020-02-19 23:21:40 -0800

    Update ActivityToday.py

--- ActivityToday.py
+++ ActivityToday.py
@@ -7,7 +7,7 @@ user_input = 'y'
 while user_input == 'y':
     result = random.choice(choices)
     print('What should you do today, you ask? Here you go: ', result)
-    user_input = str(input('Would you like to try again again? '))
+    user_input = str(input('Would you like to try again again? Y/N: '))
     user_input = user_input.lower()
     if user_input == 'y':
         True

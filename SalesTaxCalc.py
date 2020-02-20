10afe10f96cf309ecd898d141a903f13f8e00468
Patrick Go <patrickwilliamgo@gmail.com>
2020-02-20 12:24:12 -0800

    Create SalesTaxCalc.py

---
+++ SalesTaxCalc.py
@@ -0,0 +1,9 @@
+#Sales Tax Calculator
+
+print("Welcome to the Sales Tax Calculator! \n")
+
+user_input = float(input("Please enter the price of item: "))
+
+sales_tax = float(user_input*9.09)
+
+print(sales_tax)

60ea33399a2b08564a0edfe3319e6fae949afccb
Patrick Go <patrickwilliamgo@gmail.com>
2020-02-20 12:24:14 -0800

    Update WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -2,8 +2,16 @@
 
 #URL = https://realpython.com/beautiful-soup-web-scraper-python/
 import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
 
 URL = 'https://www.google.com/'
 page = requests.get(URL)
 
+soup = BeautifulSoup(page.text, 'html.parser')
+results = soup.find(id='ResultsContainer')
+soup.findAll('a')
+
 print(page)
+print(results) #Returns None, why?

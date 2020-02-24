9d4769046f04a61a9f2f900dce33cb4e5690b0d0
Patrick Go <patrickwilliamgo@gmail.com>
2020-02-23 21:30:39 -0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -6,31 +6,19 @@ words = ['apples', 'tesla', 'facebook', 'basketball', 'python']
 word = random.choice(words)
 print("Welcome to the hangman game! Let's begin..")
 
-guesses = ''
-
 turns = 5
 
-user_answer = 0 #Store variable
 
-print('Guess the chars')
+print('Guess the chars\n')
+print('The word is: ', word)
 
 while turns > 0:
-    failed = 0
-    char = str(input('Test: '))
+    char = str(input('Enter a letter for the word: '))
     for char in word:
-        if char in guesses:
-            print(char)
+        if char == word:
+            print(char, 'you got it! You won')
+            break
         else:
             print('_')
-            failed +=1
-    if failed == 0:
-        print("You won! The word is: ", word)
-        break
-        guess = str(input("Guess a char: "))
-        guesses += guess
-        if guess not in word:
-            turns -= 1
-            print("wrong!")
-            print('You have,' + turns, 'left')
-            if turns == 0:
-                print("You lose!")
+            continue
+            turns -=1

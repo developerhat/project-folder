7666e05ac7384bf8c093448a4b38a0a07a0ba25a
Patrick Go <patrickwilliamgo@gmail.com>
2020-02-25 22:32:00 -0800

    Update Feb.py

--- Feb.py
+++ Feb.py
@@ -5,6 +5,42 @@ def frames(minutes, fps):
     return minutes * fps
 
 
-def one_odd_one_even(num):
-    for i in num:
-        
+
+def googlify(n):
+    if n <= 1:
+        return 'invalid'
+    else:
+        result = 'G' + n*'o' + 'gle'
+        result = str(result)
+        return result
+
+def catch_zero_division(expr):
+    if '0' in expr:
+        return True
+    elif eval(expr) == 0:
+        return True
+    else:
+        return False
+
+#Not my code, solution code edabit
+#WTF! Looks so simple compared to mine. Wow
+def catch_zero_division(expr):
+	try: eval(expr)
+	except: return True
+	return False
+
+#Ohh shit! Got this on my own eventually!
+#TOOK SOME ITME! DAMN RUSTY AF!
+
+#WOW! Was tryna actually divide as opposed to using MODULUS
+#FUCKEN ROOKIE
+def is_leap(year):
+    if year % 400 == 0:
+        return True
+    elif year % 4 == 0:
+        if year % 100 != 0:
+            return True
+        elif year % 100 ==0:
+            return False
+    else:
+        return False

1ce799928b78404b7d3d02bf522f156c1cc739d6
Patrick Go <patrickwilliamgo@gmail.com>
2020-02-27 12:41:13 -0800

    Update login4.py

--- login4.py
+++ login4.py
@@ -1,7 +1,33 @@
 #Login 4.0 program
 
+print("\n")
+start_program = str(input("Welcome to the Login 4.0 program! Press enter to continue"))
 
-start_program = str(input("Welcome to the Login 4.0 program! Would you like to create an account? "))
+username_list = []
+password_list = []
 
-if start_program.lower() == 'y':
-    print
+print("Let's start by creating an account \n")
+
+username = str(input("Please enter username: "))
+password = input("Now, let's enter a password: ")
+username_list.append(username)
+password_list.append(password) #Storing credentials in the list
+
+print("Great! Now, let's login \n")
+
+attempts_counter = 3
+
+while attempts_counter > 0:
+    user_login = str(input("Username: "))
+    if user_login in username_list:
+        print("Username found! Please enter a password")
+        pass_login = input("Password: ")
+        if pass_login in password_list:
+            print("Authenticated! Hooray!")
+            break
+        elif pass_login not in password_list:
+            print("Invalid password. Please try again. ")
+            attempts_counter -=1
+    elif user_login not in username_list:
+        print("Invalid username. Please try again (3 attempts max): ")
+        attempts_counter -=1

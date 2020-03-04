28169b00fe66be871678aa648a9292a8386077a2
Patrick Go <patrickwilliamgo@gmail.com>
2020-03-03 21:22:49 -0800

    Update SimplePrograms.py

--- SimplePrograms.py
+++ SimplePrograms.py
@@ -7,3 +7,15 @@ def count_vowels(word):
     vowels = 'aeiou'
     word = word.lower()
     print(word.count(vowels))
+
+
+def fizz_buzz(num):
+    for i in range(num):
+        if i % 2 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        elif i % 2 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)

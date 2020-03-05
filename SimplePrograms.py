85617aa4469ddf4f7e90f4a71cd28b139c71bb77
Patrick Go <patrickwilliamgo@gmail.com>
2020-03-05 12:50:54 -0800

    Update SimplePrograms.py

--- SimplePrograms.py
+++ SimplePrograms.py
@@ -8,14 +8,14 @@ def count_vowels(word):
     word = word.lower()
     print(word.count(vowels))
 
-
+#Oh shoot it worked! Did this on my own. Had to look up reorganizing & adding FizzBuzz as first check tho
 def fizz_buzz(num):
     for i in range(num):
-        if i % 2 == 0:
-            print('Fizz')
+        if i % 2 == 0 and i % 5 == 0:
+            print("FizzBuzz")
         elif i % 5 == 0:
             print('Buzz')
-        elif i % 2 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+        elif i % 2 == 0:
+            print('Fizz')
         else:
             print(i)

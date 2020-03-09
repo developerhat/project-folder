5029445539f46e01c012dcc114016b942c95501f
Patrick Go <patrickwilliamgo@gmail.com>
2020-03-08 22:22:59 -0700

    Update SimplePrograms.py

--- SimplePrograms.py
+++ SimplePrograms.py
@@ -1,12 +1,14 @@
 #Misc simple programs
 
 
-#Counting vowels program
-
+#Counting vowels program (Incomplete)
 def count_vowels(word):
     vowels = 'aeiou'
+    count = 0
     word = word.lower()
-    print(word.count(vowels))
+    for vowel in vowels:
+        count = word.count(vowel)
+        print(count)
 
 #Oh shoot it worked! Did this on my own. Had to look up reorganizing & adding FizzBuzz as first check tho
 def fizz_buzz(num):
@@ -19,3 +21,19 @@ def fizz_buzz(num):
             print('Fizz')
         else:
             print(i)
+
+def reverse_string(str):
+    return str[::-1]
+
+#Got through this w minimal help
+def is_palindrome(n):
+    n = str(n)
+    if n[::-1] == n[0::]:
+        return True
+    else:
+        return False
+
+#Counts number of words in program, did this on my own!
+def count_words(str):
+    for i in str:
+        return str.count(' ') +1

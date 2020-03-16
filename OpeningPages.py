f3a7ffc40be6dd92b4ae925de733d5b8136f34dd
Patrick Go <patrickwilliamgo@gmail.com>
2020-03-15 21:07:15 -0700

    Update OpeningPages.py

--- OpeningPages.py
+++ OpeningPages.py
@@ -24,6 +24,7 @@ while user_answer.lower() != 'n': #Wow, this works, but it's a bit clunky. Y/N d
             True
         else:
             False
+            print('Thank you for using the Page Opener program! :) ')
     elif user_choice == 2:
         webbrowser.open('https://glassdoor.com', new =1, autoraise = True)
         user_answer = str(input('Great! Would you like to open another page? Y/N: '))
@@ -31,6 +32,7 @@ while user_answer.lower() != 'n': #Wow, this works, but it's a bit clunky. Y/N d
             True
         else:
             False
+            print('Thank you for using the Page Opener program! :) ')
     elif user_choice == 3:
         webbrowser.open('https://reddit.com', new =1, autoraise = True)
         user_answer = str(input('Great! Would you like to open another page? Y/N: '))
@@ -45,6 +47,7 @@ while user_answer.lower() != 'n': #Wow, this works, but it's a bit clunky. Y/N d
             True #How do we make it so that it just loops back to regular, without having to cancel the program?
         else:
             False
+            print('Thank you for using the Page Opener program! :) ')
     elif user_choice == 5:
         webpage_name = str(input('Name of website?: '))
         webbrowser.open('https://' + webpage_name + '.com', new =1, autoraise = True)
@@ -53,6 +56,7 @@ while user_answer.lower() != 'n': #Wow, this works, but it's a bit clunky. Y/N d
             True
         else:
             False
+            print('Thank you for using the Page Opener program! :) ')
 
     #if user_answer.lower() == 'y':
     #    continue

a0fc2899de4793f1e6d09513f80dc1db429c0db2
Patrick Go <patrickwilliamgo@gmail.com>
2020-03-19 16:41:36 -0700

    Create Count_to_n.py

---
+++ Count_to_n.py
@@ -0,0 +1,12 @@
+#Count to N Program
+
+num = int(input("Welcome to the counting to N program! Enter a number to count to: "))
+
+for i in range(num):
+    print(i)
+
+
+def count_to_n(num):
+    while x != num:
+        return print(num)
+        x += 1

8fe72a8383087d174fb4abde4a202a54d1d3167c
Patrick Go <patrickwilliamgo@gmail.com>
2020-03-19 16:41:31 -0700

    Create GuessNumber3.py

---
+++ GuessNumber3.py
@@ -0,0 +1,34 @@
+#Guess the number
+
+import random
+
+
+integer = random.randint(1,5)
+user_answer = 0 #Store variable
+
+print("Welcome to the Guess the Number Program 2.0! ")
+print("\n")
+
+#Wow got this all on my own!! Keep working
+loop_placeholder = True
+while loop_placeholder == True:
+    user_answer = int(input("I'm thinking of a number between 1 and 5, can you guess what it is? "))
+    if user_answer < integer:
+        print("Too low! Keep trying..")
+    elif user_answer > integer:
+        print("That's too high! Keep trying..")
+    elif user_answer == integer:
+        print("You got it mothafucka!! Yeee")
+        print("The number was: ", integer)
+        retry = input(str('Would you like to guess again? Y/N: '))
+        if retry.lower() == 'y':
+            loop_placeholder = True
+            integer = random.randint(1,5)
+            user_answer = int(input("Let's do it! Number between 1 and 5, can you guess what it is? "))
+            if user_answer < integer:
+                print("Too low! Keep trying..")
+            elif user_answer > integer:
+                print("That's too high! Keep trying..")
+        else:
+            loop_placeholder = False
+            print('Thanks for using the GuessNumber 2.0 program! :) ')

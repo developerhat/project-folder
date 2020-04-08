6340efb336fde3d91e9706cda1581e8a63fea7a1
Patrick Go <patrickwilliamgo@gmail.com>
2020-04-07 17:17:12 -0700

    Update LoginOpen3.py

--- LoginOpen3.py
+++ LoginOpen3.py
@@ -18,32 +18,42 @@ print("Great! Now, let's login \n")
 
 attempts_counter = 3
 #Find some way to track logout from user, so can log out. Another while loop? fuck..
-logout = 1
-
-while attempts_counter or logout > 0 :
+user_prompt =''
+re_prompt = ''
+while attempts_counter > 0:
     user_login = str(input("Username: "))
+    if user_prompt.lower() or re_prompt.lower() == 'n':
+        False
+        print("Thank you for using THE.LOGIN.PROGRAMM!!! ")
+
     if user_login in username_list:
         print("Username found! Please enter a password")
         pass_login = input("Password: ")
         if pass_login in password_list:
             print("Authenticated!")
-            user_prompt = ''
-
+            user_prompt = 'y'
             while user_prompt.lower() != 'n':
                 user_prompt = str(input('Would you like to open a new page? Y/N: '))
                 if user_prompt.lower() == 'y':
-                    site = str(input("Which website? Tsype without www & .com: "))
+                    site = str(input("Which website? Type without www & .com: "))
                     webbrowser.open('https://' + site +'.com', new =1, autoraise = True)
-                    re_prompt = str((input("Another one? Y/N: ")))
-                    if re_prompt.lower() == 'y':
-                        print("What're we opening now? ")
-                        True
-                    else:
-                        break
-                        False
+                    #re_prompt = str((input("Another one? Y/N: ")))
+                    #if re_prompt.lower() == 'y':
+                    #    True
+                    #elif re_prompt.lower() == 'n':
+                    #    False
+                    #    break
+                    #Couldn't figure this out... just removed it 
                 elif user_prompt.lower() == 'n':
                     print("Thanks for using the loginopen program! :)")
+                    print("Logging out...")
+                    attempts_counter = -3
+                    False
                     break
+                    #Somewhere here?..
+            else:
+                break
+                print("Thanks for using the loginopen programmmmm! :)")
         elif pass_login not in password_list:
             print("Invalid password. Please try again. ")
             attempts_counter -=1
@@ -52,3 +62,5 @@ while attempts_counter or logout > 0 :
         print("Invalid username. Please try again (3 attempts max): ")
         attempts_counter -=1
         print('Attempts left: ', attempts_counter)
+else:
+    print("Hi")

7cce4b2caa59ebcf7fe84e54def2e9ce6748524f
Patrick Go <patrickwilliamgo@gmail.com>
2020-04-11 12:29:00 -0700

    Create func_output.py

---
+++ func_output.py
@@ -0,0 +1,18 @@
+def function1(int):
+    result = int /2
+    return result
+
+def function2(int2):
+    result2 = int2 * 4
+    return result2
+
+def combined_func(combined):
+    combined = print('Hello! The results are: ',)
+
+
+#print(function1(40))
+#print(function2(20))
+print(combined_func(function1))
+print(combined_func(function2))
+
+#why is this returning None type?

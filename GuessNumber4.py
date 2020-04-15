de7534ff35101442bcda3fad99cbb096c2e852b2
Patrick Go <patrickwilliamgo@gmail.com>
2020-04-14 21:05:39 -0700

    Update GuessNumber4.py

--- GuessNumber4.py
+++ GuessNumber4.py
@@ -12,11 +12,15 @@ attempts = 5
 while user_guess != actual_number or attempts != 0:
     user_guess = int(input("I'm thinking of a number between 1-10. What's your guess?: "))
     if user_guess == actual_number:
-        print("Hooray! You got it :) Good shit ")
+        print("Hooray! You got it :) Good shit. Number: ", actual_number)
+        break
     else:
         attempts = attempts - 1
         if attempts < 0:
-            print("Try again!")
+            print("Try again! Number was: ", actual_number)
             break
         print("Uh oh! Try again. Attempts left: ", attempts)
-        print("Actual number: ", actual_number)
+        if user_guess < actual_number:
+            print("You're too low! Higher..")
+        elif user_guess > actual_number:
+            print("Too high! Lower ")

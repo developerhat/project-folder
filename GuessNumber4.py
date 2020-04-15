6a691a172831392bd74a999bfdf2d44f06846948
Patrick Go <patrickwilliamgo@gmail.com>
2020-04-14 17:31:09 -0700

    Create GuessNumber4.py

---
+++ GuessNumber4.py
@@ -0,0 +1,22 @@
+#Guess the number program 3.0
+import random
+
+actual_number = random.randint(0, 10)
+
+
+print("Hello! Welcome to the guess the number program! :) ")
+
+user_guess = ''
+attempts = 5
+
+while user_guess != actual_number or attempts != 0:
+    user_guess = int(input("I'm thinking of a number between 1-10. What's your guess?: "))
+    if user_guess == actual_number:
+        print("Hooray! You got it :) Good shit ")
+    else:
+        attempts = attempts - 1
+        if attempts < 0:
+            print("Try again!")
+            break
+        print("Uh oh! Try again. Attempts left: ", attempts)
+        print("Actual number: ", actual_number)

f9e1ad3879a3af0acf5731fc7bd89e94d7d16580
Patrick Go <patrickwilliamgo@gmail.com>
2020-12-04 21:41:59 -0800

    Update hangman_4.py

--- hangman_4.py
+++ hangman_4.py
@@ -1,8 +1,18 @@
-#Contact book program
+#Hangman game
 
 import random
 
 word = random.choice(['Subaru','Tesla','Honda','BMW'])
 
-
+guessed_letters = []
+attempt_number = 7
 print(word)
+
+
+while attempt_number <= 7:
+    user_input = str(input('Input a letter: '))
+    if user_input in word:
+        print("You got it!")
+    else:
+        attempt_number -= 1
+        print("Nope! Attempts left: ", attempt_number)

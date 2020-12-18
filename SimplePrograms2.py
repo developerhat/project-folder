f9574cc3918ab512b69565e6081cfc047edd78b0
Patrick Go <patrickwilliamgo@gmail.com>
2020-12-17 21:56:23 -0800

    Create SimplePrograms2.py

---
+++ SimplePrograms2.py
@@ -0,0 +1,43 @@
+#Simple Programs 2 to run scripts on
+
+
+def numbers_sum(lst):
+    nums_only = []
+    for i in lst:
+        if isinstance(i, bool):
+            continue
+        elif isinstance(i, int):
+            nums_only.append(i)
+        else:
+            continue
+    return sum(nums_only)
+
+
+
+def unique_sort(lst):
+    no_dupes = []
+    for i in lst:
+        if i not in no_dupes:
+            no_dupes.append(i)
+    return sorted(no_dupes)
+
+def count_vowels(txt):
+    vowels = 'aeiou'
+    vowel_count = 0
+    for i in txt:
+        if i in vowels:
+            vowel_count += 1
+    return vowel_count
+
+def sum_two_smallest_nums(lst):
+    two_lowest_nums = []
+    positives = []
+    for i in lst:
+        if i < 0:
+            continue
+        elif i > 0:
+            positives.append(i)
+    positives = sorted(positives)
+    two_lowest_nums.append(positives[0])
+    two_lowest_nums.append(positives[1])
+    return sum(two_lowest_nums)

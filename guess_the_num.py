a1f33a6588bb538167672254f6f1e545ee9bd860
Patrick Go <patrickwilliamgo@gmail.com>
2020-12-20 20:43:37 -0800

    Update guess_the_num.py

--- guess_the_num.py
+++ guess_the_num.py
@@ -8,12 +8,13 @@ rand_number = random.randint(1,5)
 
 user_input = int(input("I'm thinking of a number between 1-5. Can you guess what it is? "))
 
-user_attempts = 5
+user_attempts = 0
 
-while user_input != rand_number:
-    user_attempts -= 1
-    print("Oh no! You did not guess correctly. Attempts left: ", user_attempts)
-    continue
-else:
-    print("You got it right! Nice. ")
- 
+while user_attempts < 5:
+    if user_input == rand_number:
+        print("You got it right! Nice. ")
+        break
+    else:
+        user_attempts += 1
+        print("Oh no! You did not guess correctly. Attempts left: ", user_attempts)
+        True

448d93644d375e142e1290d3bb9ecb0d3e948ae9
Patrick Go <patrickwilliamgo@gmail.com>
2021-01-31 21:30:29 -0800

    Create CIS122PatrickGoLab1.py

---
+++ CIS122PatrickGoLab1.py
@@ -0,0 +1,32 @@
+#PatrickGoLab1.py
+#Author: Patrick Go
+#Date last modified: 01/31/21
+# Purpose: To demosntrate how to create your first Python Program
+
+#Program uses: to become familiar with debugging process, recognizing syntax errors, using IDLE
+
+#Part 1:
+def main():
+    print("Hello World!")
+    print("My favorite food is pizza.", " Hello Professor Ferguson!")
+    print("Isn't Python Great? ") #Use double quotation marks
+    print("Python is one of the \nmost popular programming languages")
+main()
+
+
+#Part 2:
+
+#print (“My name is _________")
+#The syntax error is with the quotations used for the print statement. This was debugged by replacing the quotation marks
+
+#print("a“ 7);
+#The error here is there is no comma to separate "a" with 7.
+
+#print(1, "b);
+#The error here is the quotation marks for b are not closed.
+
+#print(2, "c", 3);
+#The error here is there is an invalid character at the end of the print statement ;
+
+#Print “F", 5
+#The error here is print() is capitalized, and does not have a following parenthesis

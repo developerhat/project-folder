9f813096f58ad76ab604a2fd1dea354a78cae5bf
Patrick Go <patrickwilliamgo@gmail.com>
2021-01-31 21:30:27 -0800

    Create CIS122Week1Code.py

---
+++ CIS122Week1Code.py
@@ -0,0 +1,13 @@
+#Hello World.py
+#Author: Professor Ferguson
+#Date last modified: 7/23/20
+# Purpose: To demosntrate how to create your first Python Program
+
+#Every Python program must have a main function - more on functions later but
+#for now make usre the you follow this structure for every program you create
+
+
+def main():     #include this line in every program
+    print ("Hello World!")
+
+main()          #Call to main() is also required at the end of every program

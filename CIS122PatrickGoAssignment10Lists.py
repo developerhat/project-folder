a0706a0f1789d1739df99d65d976ad2ff63de61e
Patrick Go <patrickwilliamgo@gmail.com>
2021-03-28 16:50:45 -0700

    Create CIS122PatrickGoAssignment10Lists.py

---
+++ CIS122PatrickGoAssignment10Lists.py
@@ -0,0 +1,18 @@
+#PatrickGo Assignment 10.py
+#Author: Patrick Go
+#Date last modified: 03/28/21
+#Purpose: Using lists
+
+#Program uses: List comprehension practice
+
+#Assignment 10 - Lists
+
+#Problem 1:
+
+final_list = []
+
+while final_list < 7:
+    user_list = int(input("Enter a number: "))
+    final_list.append(user_list)
+
+print(final_list)
